17a3e4270836d703b777a6191b96eeae0be0b611
Felipe Miller <fmvargas@protonmail.com>
2023-07-30 14:40:17 -0300

    atividade ilha

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ const calcularNotas = (valorDesejado) => {
 
   return quantidadeNotas;
 };
+
+const valorSaque = parseFloat(prompt("Qual valor de GrowCoins você deseja sacar?"));
+
+if (!isNaN(valorSaque) && valorSaque > 0) {
+  const notas = calcularNotas(valorSaque);
+
+  document.write(`<p>GrowCoins solicitados: GC$ ${valorSaque.toFixed(2)}</p>`);
+  document.write("<p>Notas utilizadas:</p>");
+  document.write("<ul>");
+
+  for (const nota in notas) {
+    if (notas[nota] > 0) {
+      document.write(`<li>${notas[nota]} nota(s) de GC$ ${nota},00</li>`);
+    }
+  }
+
+  document.write("</ul>");
+} else {
+  document.write("<p>Não entendi! Por favor, digite um valor de GrowCoins maior que zero.</p>");
+}
